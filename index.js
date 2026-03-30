2d102599ce00ddddfe5e7e08e49219be26fed117
laiba <laibyyy43@gmail.com>
2026-03-30 11:10:19 +0500

    message

---
+++ index.js
@@ -0,0 +1,22 @@
+// let passage=document.createElement("passage");
+// passage.innerHTML="javascript is so sucks";
+
+
+
+//  let h1=document.getElementById('h1');
+// h1.innerHTML="everything is gonna so tough "
+let h1=document.createElement("h1");
+h1.innerHTML="heading tag";
+
+
+let h3=document.createElement("h3");
+
+let para=document.createElement("p");
+para.innerHTML="this is para"
+
+
+
+let main=document.getElementById("main");
+main.append(h1)
+main.append(para)
+
